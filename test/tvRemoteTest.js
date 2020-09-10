972d6b80c326cf8981cf19a225236d5c48f26c2d
chiwong08 <chiandwendy@gmail.com>
2020-09-10 16:03:06 +0100

    moved files

---
+++ test/tvRemoteTest.js
@@ -0,0 +1,24 @@
+const { exportAllDeclaration } = require("@babel/types");
+
+const {
+    tvRemote
+}
+
+describe("Sample Tests", () => {
+
+    test("example", () => {
+      expect(tvRemote("codewars")).toBe(36);
+    });
+    
+    test("misc", () => {
+      expect(tvRemote("does")).toBe(16);
+      expect(tvRemote("your")).toBe(23);
+      expect(tvRemote("solution")).toBe(33);
+      expect(tvRemote("work")).toBe(20);
+      expect(tvRemote("for")).toBe(12);
+      expect(tvRemote("these")).toBe(27);
+      expect(tvRemote("words")).toBe(25);
+    });
+    
+  });
+  
